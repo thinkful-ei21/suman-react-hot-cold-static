af81dd74a08b857f8a909d4af1400016571ade57
Suman Bandita <sumanbandita@gmail.com>
2018-06-21 11:54:19 -0400

    initial commit

--- src/components/game.js
+++ src/components/game.js
@@ -4,14 +4,17 @@ import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
+import './game.css';
 
 export default function Game(props) {
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
+            <div className="game">
+                <GuessSection feedback="Make your guess!" />
+                <GuessCount count={3} />
+                <GuessList guesses={[10, 15, 25]} />
+            </div>            
         </div>
     );
 }
--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -4,8 +4,8 @@ import './guess-count.css';
 
 export default function GuessCount(props) {
     return (
-        <p>
-            Guess #<span id="count">{props.count}</span>!
+        <p className="staticSentence">
+            Guess #<span className="count">{props.count}</span>!
         </p>
     );
 }
